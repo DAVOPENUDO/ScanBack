c873a9a3e937fc547df37aaef72c3eea992d1148
DAVOPENUDO <murillomtzjose2019b@gmail.com>
2024-11-13 01:20:25 -0600

    Actualizar proyecto con los últimos cambios pt2

--- src/models/server.ts
+++ src/models/server.ts
@@ -1,65 +1,66 @@
-import express, { Application } from "express";
+import express, {Application} from "express";
 import cors from "cors";
+
 import mysql from "../db/conect";
 
-// Imports de Rutas
+
+//Imports de Rutas 
 import profesRouter from "../routes/profes.routes";
 import alumnoRouter from "../routes/alumnos.routes";
 import authRouter from "../routes/autenticador.routes";
 import registroRouter from "../routes/registros.routes";
 
-class Server {
-    private app: Application;
-    private port: string;
-    private baseUrl: string;
+class Server{
+    
+    private app:Application;
+    private port:string;
+    
 
-    constructor() {
-        this.app = express();
-        this.port = process.env.PORT || '8000';
-        this.baseUrl = 'https://web-production-162a.up.railway.app';
-
-        // Métodos Iniciales
+    constructor(){
+        this.app=express();
+        this.port=process.env.PORT || '8000';
+        //Metodos Inicales
         this.conectarDB();
         this.middlewares();
         this.routes();
     }
-
-    listen() {
-        this.app.listen(this.port, () => {
-            console.log(`Servidor corriendo en ${this.baseUrl}:${this.port}`);
+    listen(){
+        this.app.listen(this.port, ()=>{
+            console.log(`Servidor corriendo en http://localhost:${this.port}`);
         });
     }
-
-    routes() {
-        this.app.use('/auth', authRouter);
-        this.app.use('/registro', registroRouter);
-        this.app.use('/profesores', profesRouter);
-        this.app.use('/alumnos', alumnoRouter);
+    routes(){
+        this.app.use('/auth',authRouter);
+        this.app.use('/registro',registroRouter);
+        this.app.use('/profesores',profesRouter);
+        this.app.use('/alumnos',alumnoRouter);
         console.log('Rutas Listas');
+        
     }
-
-    middlewares() {
-        // CORS
+    middlewares(){
+        //CORS
         this.app.use(cors());
-        // Lectura del Body
+        //Lectura del Body
         this.app.use(express.json());
-        // Carpeta Pública
+        //Carpeta Publica
         this.app.use(express.static('public'));
-        console.log('Middlewares Listos');
+        console.log('Middelwares Listos');
     }
-
-    conectarDB() {
-        try {
-            mysql.connect((err) => {
-                if (err) {
-                    console.log('Error al establecer la conexión'); 
+    conectarDB(){
+        try{
+            mysql.connect((err)=>{
+                if(err){
+                    console.log('Error al establecer la conexion'); 
                     mysql.on('error', this.conectarDB); 
-                } else {
-                    console.log('Conectado a la base de datos');
-                }
+                }      
+                else
+                    console.log('Conectado a la base de datos');         
             });
-        } catch (ex) {
+            
+
+        }catch(ex){
             console.log('Ha fallado el servidor');
+            
         }
     }
 }
