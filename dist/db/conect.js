aad1ff46da3a5b2b48bf09b9f331049a9cc53255
DAVID ROMÁN <utp0144150@alumno.utpuebla.edu.mx>
2024-11-14 16:18:48 -0600

    conexion base de datos

--- dist/db/conect.js
+++ dist/db/conect.js
@@ -3,12 +3,13 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const mysql_1 = __importDefault(require("mysql"));
+const mysql_1 = __importDefault(require("mysql2"));
 const conexion = mysql_1.default.createConnection({
-   host: 'junction.proxy.rlwy.net',
-   database: 'railway',
-   user: 'root',
-   password: 'pdsxvzqhYmGEHWTbyPLnSDwfjMpjncMh'
+    host: 'junction.proxy.rlwy.net',
+    port: 44125,
+    database: 'railway',
+    user: 'root',
+    password: 'pdsxvzqhYmGEHWTbyPLnSDwfjMpjncMh'
 });
 exports.default = conexion;
 /**
